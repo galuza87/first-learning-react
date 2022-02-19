3ef527feca6a40aec21b2d3e309af5e8905e15f9
galuza87 <galuza87@gmail.com>
2022-02-19 22:15:58 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          andrew <code>src/App.js</code> and save to reload.
+        </p>
+        <p> hi so lets start learning  </p>
+        <div><button id = 'increase'/></div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
